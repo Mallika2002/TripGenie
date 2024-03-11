36503ef15cbcbaeae496ac5e9146a8b5bc73f40e
Mallika Swetha <mallikaswetha2002@gmail.com>
2024-03-11 16:37:28 +0530

    Update code.py

--- streamlit/code.py
+++ streamlit/code.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 import streamlit as st
 from usellm import UseLLM, Message, Options
-from utils1 import TOUR_GUIDE_SYSTEM, TRIP_PLANNER_SYSTEM
+from utils1 import TRIP_PLANNER_SYSTEM
 from utils1 import format_trip_planner_message 
 
 from streamlit_geolocation import streamlit_geolocation
@@ -10,22 +10,6 @@ from streamlit_geolocation import streamlit_geolocation
 service = UseLLM(service_url="https://usellm.org/api/llm")
 
 st.set_page_config(layout="wide")
-
-# page_bg_img = """
-# <style>
-
-#  [data-testid="stApp"]{
-#  background-image:url('https://raw.githubusercontent.com/Mallika2002/TripGenie/main/sky.jpg');
-#  background-size:cover;
-#  }
-
-#  [data-testid="stToolbar"]{
-#  right:2rem;
-#  }
-
-# </style>
-# """
-# st.markdown(page_bg_img, unsafe_allow_html=True)
 
 video_html = """
 		<style>
@@ -46,9 +30,10 @@ video_html = """
 		  color: #f1f1f1;
 		  width: 100%;
 		  padding: 20px;
+          
 		}
 
-		</style>	
+		</style>
 		<video autoplay muted loop id="myVideo">
 		  <source src="https://static.streamlit.io/examples/star.mp4")>
 		  Your browser does not support HTML5 video.
@@ -65,32 +50,62 @@ db = client['tripdb']
 # Custom CSS for styling input box
 custom_css = """
     <style>
-        .stTextInput{
-            width:1200px !important;
+        .stTextInput input[type="text"] {
+            background-color: transparent !important;
+            border: 1px solid transparent !important; /* Set initial border color to transparent */
+            font-size: 25px !important;
+            font-weight: bold !important;
+            color: white !important;
+            position: absolute !important;
+            z-index: 1 !important; /* Ensure the text input field appears over the video */
+            transition: border-color 0.3s ease; /* Add smooth transition for border color change */
         }
-        .stTextInput input {
-            color: black !important;
-            background-color: lightyellow !important;
-            border:1px solid black !important;
+
+        .stTextInput input[type="text"]:focus {
+            border-color: white !important; /* Change border color to white when the input is focused */
+        }
+        .st-c0{
+        width:500px !important;
+        }
+        .st-b0{
+        width:500px !important;
+        }
+        .st-dh{
+        width:500px !important;
+        }
+        .st-cr st-cs st-ct st-d8 st-d9 .st{
+    transform: translate3d(79px, 762px, 0px);
+}
+        .stTextInput {
+            width: 400px !important;
+            height: 90px !important;
+        }
+        div[data-testid="element-container"] p{
+            font-size:20px;
+        }
+        div[data-testid="element-container"] li{
+            font-size:20px;
         }
         .stApp-container{
-        margin-left:35px;
+            margin-left:35px;
         }
         
     </style>
 """
 st.markdown(custom_css, unsafe_allow_html=True)
 
-def tour_guide_section():
-    st.title(r"$\textsf{\Large Tour Guide Assistant}$")
-    user_input = st.text_input(r"$\textsf{\normalsize Enter the Place You Want to Visit}$", key="input1")
-    if st.button(r"$\textsf{\normalsize Send}$", key="button1"):
-        if user_input:
-            system_message = TOUR_GUIDE_SYSTEM
-            output = get_response(system_message, user_input)
-            st.markdown(output.content)
-        else:
-            st.markdown("Please Enter Some Text")
+# def tour_guide_section():
+#     st.title(r"$\textsf{\Large Tour Guide Assistant}$")
+#     user_input = st.text_input(r"$\textsf{\normalsize Enter the Place You Want to Visit}$", key="input1")
+#     if st.button(r"$\textsf{\normalsize Send}$", key="button1"):
+#         if user_input:
+#             system_message = TOUR_GUIDE_SYSTEM
+#             output = get_response(system_message, user_input)
+#             st.markdown(output.content)
+#         else:
+#             st.markdown("Please Enter Some Text")
+
+
 
 def update_sidebar_user(user_input):
     if user_input:
@@ -107,39 +122,52 @@ def update_sidebar_user(user_input):
                 trips = user_data.get("trips", [])
                 if "trips" in user_data:
                         trips = user_data["trips"]
-                        st.sidebar.header("Your trips:")
+                        st.sidebar.markdown('<div style="margin-left:5px;margin-bottom:10px;color: pink;font-weight: bold;font-size:43px;">TripGenie</div>', unsafe_allow_html=True)
+                        st.sidebar.markdown('<div style="margin-left:13px;margin-top:30px;margin-bottom:10px;color: white;font-size:23px;">Your Trips:</div>', unsafe_allow_html=True)
+
+
                         for trip in trips:
                             st.sidebar.markdown(f'<div class="place-block" style="text-align: center;">{trip}</div>', unsafe_allow_html=True)
                 else:
+                    st.sidebar.markdown('<div style="margin-left:5px;margin-bottom:10px;color: pink;font-weight: bold;font-size:43px;">TripGenie</div>', unsafe_allow_html=True)
                     st.sidebar.header("Your trips:")
             else:
+                # st.sidebar.markdown('<div style="margin-left:5px;margin-bottom:10px;color: pink;font-weight: bold;font-size:43px;">TripGenie</div>', unsafe_allow_html=True)
                 st.sidebar.header("Your trips:")
 
 
     custom_css = """
         <style>
 
-
+        
         .place-list {
             display: flex;
             flex-wrap: wrap;
 
         }
-            [data-testid="stSidebar"]{
-            
-            padding-left:30px;
-            width:280px !important;
-            }
-            .place-block {
-            background-color: #eaf9e2; /* Subtle green color */
-            padding: 5px;
-            font-size:20px;
-            margin: 5px;
-            display:inline-block;
-            border-radius: 8px;
-            width:130px; /* Set a fixed width for each block */
-            color:black;
-            }
+        [data-testid="stSidebar"]{
+        padding-left:30px;
+        width:300px !important;
+        background-image:url('https://images.unsplash.com/photo-1583161443085-2e2947a6664c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fHRyYXZlbGxpbmd8ZW58MHx8MHx8fDA%3D');
+        background-size:cover;
+        width:350px !important;
+        
+        }
+        .st-emotion-cache-16txtl3 {
+        padding: 4rem 1rem;
+        }
+
+        .place-block {
+        margin-bottom:10px;
+        background-color: #eaf9e2; /* Subtle green color */
+        padding: 5px;
+        font-size:20px;
+        margin: 5px;
+        display:inline-block;
+        border-radius: 8px;
+        width:130px; /* Set a fixed width for each block */
+        color:black;
+        }
         </style>
     """
 
@@ -171,19 +199,23 @@ def trip_planner_section():
         unsafe_allow_html=True
     )
 
-
+    
+     
     user_input = st.text_input(r"$\textsf{\normalsize Enter the Place You Want to Visit}$", key="input2")
     days = st.text_input(r"$\textsf{\normalsize Enter the Number of Days}$", key="input3")
     budget = st.text_input(r"$\textsf{\normalsize Enter the Budget per person}$", key="input4")
     
-    if st.button(r"$\textsf{\normalsize Send}$", key="button2"):
+    conditions = st.selectbox(r"$\textsf{\normalsize Select Conditions}$", ["Couple", "Friends", "Family"], key="conditions")
+
+
+    if st.button(r"$\textsf{\normalsize Generate Itinerary}$", key="button2"):
         
         if user_input:
             if len(username)>0:
                 user_data = db.users.find_one({"username": username})
                 if user_data:
                     user_location = user_data["location"].get("address", "Unknown Address")
-                    system_message = TRIP_PLANNER_SYSTEM.format(days=days, budget=budget, user_location=user_location, user_input=user_input)
+                    system_message = TRIP_PLANNER_SYSTEM.format(days=days, budget=budget, user_location=user_location, user_input=user_input,conditions=conditions)
                     output = get_response(system_message, user_input)
                     st.markdown(output.content)
 
@@ -196,7 +228,7 @@ def trip_planner_section():
             else:
 
                 # user_location = "Visakhapatnam, India"
-                system_message = TRIP_PLANNER_SYSTEM.format(days=days, budget=budget, user_location=user_location, user_input=user_input)
+                system_message = TRIP_PLANNER_SYSTEM.format(days=days, budget=budget, user_location=user_location, user_input=user_input,conditions=conditions)
                 output = get_response(system_message, user_input)
                 st.markdown(output.content)
 
@@ -221,15 +253,8 @@ def main():
     user_data_path = os.path.join(os.path.dirname(__file__), '..', 'user_data.txt')
 
     if os.stat(user_data_path).st_size == 0:
-
-        tab1, tab2 = st.tabs([r"$\textsf{\Large Tour Guide}$", r"$\textsf{\Large Trip Planner}$"])
-        with tab1:
-            output = tour_guide_section()
-            show(output)
-
-        with tab2:
-            output = trip_planner_section()
-            show(output)
+        output=trip_planner_section()
+        show(output)
 
     else:
     
@@ -238,14 +263,7 @@ def main():
             
         if username:
             update_sidebar_user("")
-
-        tab1, tab2 = st.tabs([r"$\textsf{\Large Tour Guide}$", r"$\textsf{\Large Trip Planner}$"])
-        with tab1:
-            output = tour_guide_section()
-            show(output)
-
-        with tab2:
-            output = trip_planner_section()
+            output=trip_planner_section()
             show(output)
 
 
